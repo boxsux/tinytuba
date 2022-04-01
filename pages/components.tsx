f0824d706b36912f8920fc6d998f51892bfb19cc
Box <j@box.lol>
2022-04-01 10:15:40 -0400

    Remove TubaPlayer component

--- pages/components.tsx
+++ pages/components.tsx
@@ -4,23 +4,10 @@ import Image from 'next/image';
 export const Tuba = () => {
     return (
         <main className={styles.main}>
-            <Image src="/assets/tuba.jpg" alt="tuba" width="60" height="48" id="tubaImg" />
+            <Image src="/assets/tuba.jpg" alt="tuba" width="60" height="48" id="tubaImg" className={styles.tuba} />
             <audio id="tuba">
                 <source src="/assets/tuba.mp3" type="audio/mp3" />
             </audio>
         </main>
-    );
-};
-
-export const TubaPlayer = () => {
-    return (
-        <script>
-            $(document).on('ready', (){' '}
-            {$('#tubaImg').on('click', function () {
-                $('#tuba').load();
-                $('#tuba').play();
-            })}
-            );
-        </script>
     );
 };
